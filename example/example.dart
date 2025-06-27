d0db1ee2230944b9368d26f6d373537b06fc7988
raihan sikdar <raihansikdar10@gmail.com>
2025-06-27 21:31:28 +0600

    change example

---
+++ example/example.dart
@@ -0,0 +1,25 @@
+// This is just an example to show how to use the CLI tool from terminal.
+// Save this file as example/example.dart
+
+/// To use this CLI tool after activating:
+///
+/// dart pub global activate raihan_cli
+///
+/// Then you can run:
+///
+/// raihan_cli <feature_name>
+/// raihan_cli remove <feature_name>
+///
+/// Example:
+///   raihan_cli product
+///   raihan_cli remove product
+///
+/// If you want to activate from GitHub:
+/// dart pub global activate --source git https://github.com/raihansikdar/raihan_cli.git
+///
+/// To deactivate:
+/// dart pub global deactivate raihan_cli
+///
+/// Check path:
+///   which raihan_cli  # macOS/Linux
+///   where raihan_cli  # Windows
